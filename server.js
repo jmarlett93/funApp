aa8d30cea51049a30a50163de03e93bb8251d4ae
John Marlett <1fiyero@gmail.com>
2016-09-21 14:21:15 -0500

    building angular app package

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let express = require('express')
+let app = express();
+let port = process.env.PORT || 3000;
+ 
+app.use(express.static(__dirname + '../../public'));
+app.listen(port);
