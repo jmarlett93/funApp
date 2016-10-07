35f35b3b7009cecd51160a229289360997971368
John Marlett <1fiyero@gmail.com>
2016-10-07 18:40:59 -0500

    data binding on resource controller

--- app/controllers.js
+++ app/controllers.js
@@ -6,8 +6,7 @@ angular.module('myOdysseyControllers', [])
     $scope.healthMessage = rulesMessagesService.rules[0];
     $scope.resolveMessage = rulesMessagesService.rules[1];
     $scope.areteMessage = rulesMessagesService.rules[2];
-    $scope.responsiveElems = rulesMessagesService.responsiveElems;
-    $scope.showActiveElem = rulesMessagesService.showActiveElem;
+    $scope.menMessage = rulesMessagesService.rules[3];
 })
 
 .controller('aboutController', function($scope){
@@ -16,6 +15,6 @@ angular.module('myOdysseyControllers', [])
 
 .controller('gameController', function($scope, heroBuilderService){
    
-   $scope.hero = heroBuilderService.hero.name;
-    
+   $scope.heroStats = heroBuilderService.hero;
+   
 })
--- app/services.js
+++ app/services.js
@@ -3,60 +3,38 @@
 angular.module('myOdysseyServices', [])
 
 .factory('heroBuilderService', function(){
-    
-    let heroBuilder = {};    
         
-    let heroList = [
-       {name: 'Odysseus',
-        strength: 6,
-        speed: 7,
-        resolve: 10,
-        power: 'wily' },
-       
-       {name: 'Achilles',
-        strength: 10,
-        speed: 8,
-        power: 'fatalist' }
-        ];
+    let heroObject = {
+        "health": 90,
+        "resolve": 100,
+        "arete": 80,
+        "men": 200, }
         
     return {
-        hero : heroList[0]
+        hero : heroObject
     }
 })
 
 .factory('rulesMessagesService', function(){
     
-    let rules = [ "Health: This one is simple-  if you run out of health, you die." +
-                   " Ways to gain health include doing something restful or simply" +
+    let rules = [ "If you run out of health, you die." +
+                   " Gain health by doing something restful or" +
                     " avoiding conflict for a round to recharge",
-                    "Resolve: Resolve represents your hero's will power to return" +
-                    " home to his loving wife Penelope. Too much comfort, a goddess's" +
-                    " spell or taking too many diversions, might cause your hero" +
-                    " to give up the journey home. Win battles and travel homeward to increase resolve.",
-                    "Arete: Arete represents a distinctly Greek sense of heroic qualities." +
-                    " Running out of Arete will not kill you outright, but if you are too low, your men will"+
-                    " abandon you, recruits will be hard to come by and the worst may happen should you arrive"+
-                    " home to Penelope without enough Arete."
+                    "Odysseus's will power to return" +
+                    " home. Comforts, a goddess's" +
+                    " spell or military diversions, might cause him" +
+                    " to forsake the journey.",
+                    "A distinctly Greek sense of heroic excellence." +
+                    " Losing Arete will not kill you outright, but you will disastrously lose"+
+                    " the respect of the Hellenic world.",
+                    "Some brawn always helps. You don't need them but keep them around."
                   ];
     
             let responsiveElems = [ "showIntro", "showHealth", "showResolve", "showArete"];
-    
-    let showActiveElem = function(elem = ''){
 
-        responsiveElems.forEach(function(arrayItem, elem){
-            if( arrayItem === elem){
-                elem = true;
-            }
-            else 
-            elem = false;
-            return elem;
-        })
-    }
                   
    return {
        rules: rules,
-       responsiveElems: responsiveElems,
-       showActiveElem: showActiveElem
    }
 })
 
