b581dc1329014b70a90cb5c1371f215ce504da61
John Marlett <1fiyero@gmail.com>
2016-10-07 09:58:09 -0500

    ng-click functionality

--- app/controllers.js
+++ app/controllers.js
@@ -2,8 +2,12 @@
 
 angular.module('myOdysseyControllers', [])
 
-.controller('homeController', function($scope){
-    
+.controller('homeController', function($scope, rulesMessagesService){
+    $scope.healthMessage = rulesMessagesService.rules[0];
+    $scope.resolveMessage = rulesMessagesService.rules[1];
+    $scope.areteMessage = rulesMessagesService.rules[2];
+    $scope.responsiveElems = rulesMessagesService.responsiveElems;
+    $scope.showActiveElem = rulesMessagesService.showActiveElem;
 })
 
 .controller('aboutController', function($scope){
--- app/services.js
+++ app/services.js
@@ -22,4 +22,41 @@ angular.module('myOdysseyServices', [])
     return {
         hero : heroList[0]
     }
+})
+
+.factory('rulesMessagesService', function(){
+    
+    let rules = [ "Health: This one is simple-  if you run out of health, you die." +
+                   " Ways to gain health include doing something restful or simply" +
+                    " avoiding conflict for a round to recharge",
+                    "Resolve: Resolve represents your hero's will power to return" +
+                    " home to his loving wife Penelope. Too much comfort, a goddess's" +
+                    " spell or taking too many diversions, might cause your hero" +
+                    " to give up the journey home. Win battles and travel homeward to increase resolve.",
+                    "Arete: Arete represents a distinctly Greek sense of heroic qualities." +
+                    " Running out of Arete will not kill you outright, but if you are too low, your men will"+
+                    " abandon you, recruits will be hard to come by and the worst may happen should you arrive"+
+                    " home to Penelope without enough Arete."
+                  ];
+    
+            let responsiveElems = [ "showIntro", "showHealth", "showResolve", "showArete"];
+    
+    let showActiveElem = function(elem = ''){
+
+        responsiveElems.forEach(function(arrayItem, elem){
+            if( arrayItem === elem){
+                elem = true;
+            }
+            else 
+            elem = false;
+            return elem;
         })
+    }
+                  
+   return {
+       rules: rules,
+       responsiveElems: responsiveElems,
+       showActiveElem: showActiveElem
+   }
+})
+
